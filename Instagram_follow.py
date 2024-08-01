64db228d20f8cbd4d654bf3829f993f5eb80ee30
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-08-01 23:28:45 +0500

    Update Instagram_follow.py

--- Instagram_follow.py
+++ Instagram_follow.py
@@ -9,49 +9,67 @@ class InstaBot:
         self.driver = webdriver.Chrome()
         self.username = username
         self.password = password
+
+        self.login()
+        self.search_user(search)
+
+    def login(self):
         self.driver.get("https://instagram.com")
         sleep(2)
-        self.driver.find_element_by_name("username")\
-            .send_keys(username)
-        self.driver.find_element_by_name("password")\
-            .send_keys(password)
-        self.driver.find_element(By.XPATH, '//button[@type="submit"]')\
-            .send_keys(Keys.ENTER)
+
+        # Log in to Instagram
+        self.driver.find_element(By.NAME, "username").send_keys(self.username)
+        self.driver.find_element(By.NAME, "password").send_keys(self.password)
+        self.driver.find_element(By.XPATH, '//button[@type="submit"]').send_keys(Keys.ENTER)
         sleep(3)
-        self.driver.find_element(By.XPATH, '//button[text()="Not Now"]')\
-            .send_keys(Keys.ENTER)
-        sleep(3)
-        self.driver.find_element(By.XPATH, '//button[text()="Not Now"]') \
-            .send_keys(Keys.ENTER)
-        sleep(3)
-        self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')\
-            .send_keys(search)
+
+        # Handle pop-ups
+        self.handle_popups()
+
+    def handle_popups(self):
+        try:
+            not_now_button = self.driver.find_element(By.XPATH, '//button[text()="Not Now"]')
+            not_now_button.click()
+            sleep(3)
+        except Exception as e:
+            print("No 'Not Now' button found: ", e)
+
+        try:
+            turn_on_notifications_button = self.driver.find_element(By.XPATH, '//button[text()="Not Now"]')
+            turn_on_notifications_button.click()
+            sleep(3)
+        except Exception as e:
+            print("No 'Turn on Notifications' button found: ", e)
+
+    def search_user(self, search):
+        search_box = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+        search_box.send_keys(search)
         sleep(4)
-        self.driver.find_element(By.CLASS_NAME, 'z556c')\
-            .click()
+
+        # Click on the first result
+        first_result = self.driver.find_element(By.CLASS_NAME, 'z556c')
+        first_result.click()
         sleep(5)
 
     def bulk_follow(self):
-        self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a')\
-            .send_keys(Keys.ENTER)
+        self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a').send_keys(Keys.ENTER)
         sleep(3)
-        scroll_box = self.driver.find_element_by_css_selector("div[class='isgrP']")
-        #Add the number of following of the user
-        scrolling_times = (numberoffollowing / 4)
-        scroll = 0
-        scroll_count = scrolling_times + 5  # You can use your own logic to scroll down till the bottom
-        while scroll < scroll_count:
-            self.driver.execute_script(
-                    'arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;',
-                    scroll_box)
+
+        scroll_box = self.driver.find_element(By.CSS_SELECTOR, "div[class='isgrP']")
+        
+        number_of_following = 100  # Set the number of followings here
+        scrolling_times = (number_of_following / 4)
+        scroll_count = int(scrolling_times + 5)
+
+        for _ in range(scroll_count):
+            self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', scroll_box)
             sleep(2)
             buttons = self.driver.find_elements(By.XPATH, '//button[text()="Follow"]')
             for btn in buttons:
                 if btn.text == 'Follow':
                     self.driver.execute_script("arguments[0].click();", btn)
                     sleep(1)
-            scroll += 1
 
-#Add the username, password and usernameforsearch
-Bot = InstaBot('username', 'password', 'usernameforsearch')
-Bot.bulk_follow()
+# Add the username, password, and search term
+bot = InstaBot('your_username', 'your_password', 'search_term')
+bot.bulk_follow()
